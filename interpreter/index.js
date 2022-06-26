5f2cfdd686f3531909e5c99a7807f542f627e747
Tiago <tiagoporangaba@gmail.com>
2022-06-26 01:30:34 +0000

    Adding LT, GT, EQ, AND and OR

--- interpreter/index.js
+++ interpreter/index.js
@@ -4,6 +4,11 @@ const DIV = 'DIV';
 const MUL = 'MUL';
 const SUB = 'SUB';
 const PUSH = 'PUSH';
+const LT = 'LT';
+const GT = 'GT';
+const EQ = 'EQ';
+const AND = 'AND';
+const OR = 'OR';
 
 class Interpreter {
   constructor() {
@@ -36,6 +41,11 @@ class Interpreter {
           case SUB:
           case MUL:
           case DIV:
+          case LT:
+          case GT:
+          case EQ:
+          case AND:
+          case OR:
             const a = this.state.stack.pop();
             const b = this.state.stack.pop();
 
@@ -45,11 +55,16 @@ class Interpreter {
             if (opCode === SUB) result = a - b;
             if (opCode === MUL) result = a * b;
             if (opCode === DIV) result = a / b;
-
+            if (opCode === LT) result = a < b ? 1 : 0;
+            if (opCode === GT) result = a > b ? 1 : 0;
+            if (opCode === EQ) result = a === b ? 1 : 0;
+            if (opCode === AND) result = a && b;
+            if (opCode === OR) result = a || b;
+            
             this.state.stack.push(result);
             break;
-           
-           default:
+          
+            default:
               break;
           }
       } catch (error) {
@@ -76,3 +91,23 @@ console.log('Result 3 MUL 2: ', result);
 code = [PUSH, 2, PUSH, 3, DIV, STOP];
 result = new Interpreter().runCode(code)
 console.log('Result 3 DIV 2: ', result);
+
+code = [PUSH, 2, PUSH, 3, GT, STOP];
+result = new Interpreter().runCode(code)
+console.log('Result 3 GT 2: ', result);
+
+code = [PUSH, 2, PUSH, 3, LT, STOP];
+result = new Interpreter().runCode(code)
+console.log('Result 3 LT 2: ', result);
+
+code = [PUSH, 2, PUSH, 3, EQ, STOP];
+result = new Interpreter().runCode(code)
+console.log('Result 3 EQ 2: ', result);
+
+code = [PUSH, 1, PUSH, 0, AND, STOP];
+result = new Interpreter().runCode(code)
+console.log('Result 0 AND 1: ', result);
+
+code = [PUSH, 1, PUSH, 0, OR, STOP];
+result = new Interpreter().runCode(code)
+console.log('Result 0 OR 1: ', result);
